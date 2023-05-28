b36c9a33510b11a7268e8388288f05ca263a0cbf
Alex <alexstefciornei22@gmail.com>
2023-05-28 18:37:33 +0300

    implement parser monad

--- Parser.hs
+++ Parser.hs
@@ -1,8 +1,12 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+{-# HLINT ignore "Use lambda-case" #-}
 module Parser (parse_expr, parse_code) where
 
 import Control.Monad
 import Control.Applicative
 import Expr
+import Data.Char
 
 -- Parser data type
 newtype Parser a = Parser {
@@ -12,8 +16,11 @@ newtype Parser a = Parser {
 --- type declaration ---
 
 instance Monad Parser where
-    return = undefined
-    (>>=) = undefined
+    return x = Parser $ \s -> Just (x, s)
+    mp >>= f = Parser $
+        \s -> case parse mp s of
+            Nothing -> Nothing
+            Just (x, rest) -> parse (f x) rest
 
 instance Applicative Parser where
     pure x = return x
@@ -27,11 +34,85 @@ instance Functor Parser where
         x <- px
         return $ f x
 
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  p1 <|> p2 = Parser $ \s -> case parse p1 s of
+                                Nothing -> parse p2 s
+                                ok -> ok
 --- type declaration over ---
+
+predicateParser :: (Char -> Bool) -> Parser Char
+predicateParser p = Parser $
+  \s -> case s of
+          [] -> Nothing
+          (x:xs) -> if p x then Just (x, xs) else Nothing
+
+plusParser :: Parser a -> Parser [a]
+plusParser p = do 
+    x <- p
+    whiteSpaceParser
+    xs <- starParser p
+    return (x:xs)
+
+starParser :: Parser a -> Parser [a]
+starParser p = plusParser p <|> return []
+
+variableParser :: Parser String
+variableParser = do
+    x <- predicateParser isAsciiLower
+    xs <- starParser $ predicateParser isAsciiLower
+    return (x:xs)
+
+whiteSpaceParser :: Parser String
+whiteSpaceParser = starParser (predicateParser (== ' '))
+
+openBracketParser :: Parser String
+openBracketParser = starParser (predicateParser (== '('))
+
+closeBracketParser :: Parser String
+closeBracketParser = starParser (predicateParser (== ')'))
+
+variableExprParser :: Parser Expr
+variableExprParser = Variable <$> variableParser
+
+functionExprParser :: Parser Expr
+functionExprParser = do
+    openBracketParser
+    predicateParser (== '\\')
+    v <- variableParser
+    predicateParser (== '.')
+    res <- Function v <$> exprParser
+    closeBracketParser
+    return res
+
+applicationBracketParser :: Parser Expr
+applicationBracketParser = do
+    predicateParser (== '(')
+    x <- applicationExprParser
+    predicateParser (== ')')
+    return x
+
+simpleExprParser :: Parser Expr
+simpleExprParser = applicationBracketParser <|> functionExprParser <|> variableExprParser
+
+applicationParser :: Parser Expr
+applicationParser = do
+    x <- simpleExprParser
+    whiteSpaceParser
+    y <- plusParser simpleExprParser
+    return (foldl Application x y)
+
+applicationExprParser :: Parser Expr
+applicationExprParser = applicationBracketParser <|> applicationParser
+
+exprParser :: Parser Expr
+exprParser = applicationExprParser <|> functionExprParser <|> variableExprParser
 
 -- TODO 2.1. parse a expression
 parse_expr :: String -> Expr
-parse_expr = undefined
+parse_expr str = case parse exprParser str of
+    Just (x, xs) -> x
+    _ -> error "parse error"
 
 -- TODO 4.2. parse code
 parse_code :: String -> Code
